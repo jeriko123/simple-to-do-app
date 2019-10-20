3ecc5736fc206718a2cdd6953438665c54caecfe
Yaroslav Kaniev <y.kaniev@gmail.com>
2019-10-20 16:20:34 +0300

    Updates

---
+++ precache-manifest.e9fca8371e6abb6072b5ae60c0ed2ef0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb160f74041fca4d56c4a9b0094bab5",
+    "url": "/simple-to-do-app/index.html"
+  },
+  {
+    "revision": "ad57ee6bd8f5c01a8309",
+    "url": "/simple-to-do-app/static/css/main.148a6fef.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/simple-to-do-app/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "ad57ee6bd8f5c01a8309",
+    "url": "/simple-to-do-app/static/js/main.8f87a8a0.chunk.js"
+  },
+  {
+    "revision": "01a03b7130f2db6d5da7",
+    "url": "/simple-to-do-app/static/js/runtime~main.0ac1fe95.js"
+  }
+]);
