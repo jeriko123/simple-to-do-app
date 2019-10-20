e83e7aa56ff67321473b770b08843d00845ecfaa
Yaroslav Kaniev <y.kaniev@gmail.com>
2019-10-20 17:43:27 +0300

    merge fix

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -28,11 +28,7 @@ export default class AddItem extends Component {
         className='item-add-form d-flex'
         onSubmit={this.onSubmit}
       >
-<<<<<<< HEAD
         <div className="col-auto" style={{ minWidth: 290}}>
-=======
-        <div className="col-auto">
->>>>>>> 33e1480d1248ac1b214452b277903a7aa4aea5a4
           <input
             type='text'
             className='form-control'
